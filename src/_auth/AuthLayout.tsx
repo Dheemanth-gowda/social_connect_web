1951f80ca6f7e90f1016d0666fc70c84d37b44cd
Dheemanth M D <dheemanthmd12@gmail.com>
2023-11-25 20:16:59 +0530

    assets added and shadcn configured (#2)

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,9 @@
+import React  from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
+
+  // TODO : Replace the const variable with logic of auth.
   const isAuthenticated = false;
   return (
     <>
@@ -8,9 +11,14 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
