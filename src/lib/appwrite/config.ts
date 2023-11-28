603dd6ffecd1df087becb80189e89cfe074e6683
Dheemanth-gowda <dheemanthmd12@gmail.com>
2023-11-29 00:15:17 +0530

    chore: adding Appwrite configuration

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,21 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  url: import.meta.env.VITE_PROJECT_URL,
+  dataBaseId:import.meta.env.VITE_PROJECT_DB_ID,
+  savesCollectionId:import.meta.env.VITE_PROJECT_DB_COLLECTION_SAVES,
+  userCollectionId:import.meta.env.VITE_PROJECT_DB_COLLECTION_USERS,
+  postCollectionId:import.meta.env.VITE_PROJECT_DB_COLLECTION_POSTSs
+};
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatar = new Avatars(client);
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type InewUser = {
+    name: string,
+    username: string,
+    password: string,
+    email: string
+}
